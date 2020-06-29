529d1ad3135c199793eea293799569dd6fe1216f
Joel Moss <joel@developwithstyle.com>
2020-06-29 14:44:28 +0100

    fix: Full path to worker autoload

--- lib/sidekiq/sequence.rb
+++ lib/sidekiq/sequence.rb
@@ -8,6 +8,6 @@ require 'sidekiq/sequence/engine'
 module Sidekiq
   module Sequence
     autoload :Base, 'sidekiq/sequence/base'
-    autoload :Worker, 'sequence/worker'
+    autoload :Worker, 'sidekiq/sequence/worker'
   end
 end
